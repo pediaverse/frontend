8d4d02d4430d9388680e1a6ffb1df14f183fe48b
delvinsaji <ikapp845@gmail.com>
2023-06-02 21:21:04 +0530

    jj

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,7 +4,7 @@ import "./Main.css";
 import Center from "./Center/Center";
 import Second from "./Second/Second";
 import Third from "./Third/Third";
-import Fourth from "./Fourth/Fourth";
+// import Fourth from "./Fourth/Fourth";
 import Footer from "./Footer/Footer";
 import Fifth from "./Fifth/Fifth";
 
