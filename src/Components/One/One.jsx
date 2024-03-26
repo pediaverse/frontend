748416ba338fbd570631a291225764eda83fb5a9
Sunil Kumar Bhakhar <sunilbhakhar333@gmail.com>
2024-03-26 12:02:51 +0530

    one.jsx completed

---
+++ src/Components/One/One.jsx
@@ -0,0 +1,14 @@
+import './One.css'
+
+export default function One(){
+    return <>
+    <div class="number1" id="num1">
+        <img src="http://surl.li/rvyxy" alt="" id="num1img" />
+        <div class="num1textdiv" id="num1contentdiv">
+            <p class="num1text" id="num1content">We act as a companion to each child, from kindergarten to college, and
+                help them join the <span id="spantxt">top 1% club</span> of the most skilled and successful people in their chosen field.
+            </p>
+        </div>
+    </div>
+    </>
+}
