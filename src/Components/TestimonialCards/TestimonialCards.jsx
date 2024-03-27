7e5cdfb48aa25fcec3fcf9f92820dd31bcdd8c40
71203mayank <mayank71203@gmail.com>
2024-03-28 01:26:33 +0530

    testimonial

---
+++ src/Components/TestimonialCards/TestimonialCards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './TestimonialCards.module.css'
+export default function TestimonialCards(props) {
+    console.log(props)
+  return (
+    <div key={props.id} className={styles['testimonial-card']} style={{width:`${props.width}`}}>
+        <div className={styles['card-inner-container']}>
+            <div className={styles['card-user-info']}>
+                <div className={styles['card-user-image-container']}>
+                    <img className={styles['card-user-image']} alt='user-img' src={props.user_img}></img>
+                </div>
+                <div className={styles['card-user-name']}>
+                    <div style={{color:'#2C2A50',fontSize:'20px', fontWeight:'900', lineHeight:'26px'}}>{props.name}</div>
+                    <div style={{color:'#AAB0D8',fontSize:'17px', fontWeight:'600',lineHeight:'22px'}}>{props.user_id}</div>
+                </div>
+            </div>
+            <div className={styles['user-testimonial']}>
+                {props.testimonial}
+            </div>
+        </div>
+    </div>
+  )
+}
