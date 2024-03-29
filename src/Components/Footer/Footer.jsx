d5ffbab5621beba57a096a15b1feed87a85c9718
Sunil Kumar Bhakhar <sunilbhakhar333@gmail.com>
2024-03-29 20:18:44 +0530

    footer section added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import './Footer.css'
+
+
+export default function Footer(){
+    return (
+        <>
+        <footer>
+        <div id="container" class="common">
+            <ul class="links-list">
+                    <li class="links-list-title">Company</li>
+                    <li class="links-list-items"><a href="">About us</a></li>
+                    <li class="links-list-items"><a href="">Press</a></li>
+                    <li class="links-list-items"><a href="">Careers</a></li>
+                    <li class="links-list-items"><a href="">Engineering</a></li>
+                    <li class="links-list-items"><a href="">Accessibility</a></li>
+            </ul>
+            <ul class="links-list">
+                    <li class="links-list-title">Resources</li>
+                    <li class="links-list-items"><a href="">Big Ideas</a></li>
+                    <li class="links-list-items"><a href="">Points</a></li>
+                    <li class="links-list-items"><a href="">Resources</a></li>
+                    <li class="links-list-items"><a href="">Training</a></li>
+                    <li class="links-list-items"><a href="">Remote Learning</a></li>
+            </ul>
+            <ul class="links-list">
+                    <li class="links-list-title">Support</li>
+                    <li class="links-list-items"><a href="">Helpdesk</a></li>
+                    <li class="links-list-items"><a href="">Contact</a></li>
+                    <li class="links-list-items"><a href="">Cookies Setting</a></li>
+                    <li class="links-list-items"><a href="">Privacy Center</a></li>
+            </ul>
+            <ul class="links-list">
+                    <li class="links-list-title">Community</li>
+                    <li class="links-list-items"><a href="">Teacher Community</a></li>
+                    <li class="links-list-items"><a href="">Facebook</a></li>
+                    <li class="links-list-items"><a href="">Twitter</a></li>
+                    <li class="links-list-items"><a href="">Instagram</a></li>
+            </ul>
+        </div>
+        <div id="foot-nav">
+            <ul class="nav-list">
+                <li class="nav-list-items"><a href="">&copy;Pediaverse</a></li>
+                <li class="nav-list-items"><a href="">Terms of Service</a></li>
+                <li class="nav-list-items"><a href="">Product Privacy Policy</a></li>
+                <li class="nav-list-items"><a href="">Website Privacy Policy</a></li>
+            </ul>
+        </div>
+    </footer>
+        </>
+    );
+}
