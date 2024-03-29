cde3170e258d5a7d3716187db5b8b9d995f7772f
71203mayank <mayank71203@gmail.com>
2024-03-29 15:48:14 +0530

    added navbar

--- src/Components/About/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import styles from './About.module.css'
-export default function About() {
-  return (
-    <div className={styles['about']}>About</div>
-  )
-}
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Home.module.css'
+import Navbar from '../Navbar/Navbar'
 
 
 export default function Home() {
   return (
     <div className={styles['home']}>
+        <Navbar/>
         <div className={styles['home-heading']}>
             <div className={styles['home-main-heading']}>Do what you love, Love what you do</div>
             <div className={styles['home-sub-heading']}>Discover and pursue your <span style={{color:'#28CD88'}}>passion</span> in life</div>
