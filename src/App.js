73b22b0452dde2e853c5f7a466d915907d0760bb
71203mayank <mayank71203@gmail.com>
2024-03-29 21:20:20 +0530

    merged footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from "./Components/Home/Home";
 import One from "./Components/One/One.jsx";
 import Testimonial from "./Components/Testimonial/Testimonial.jsx";
-// import TestimonialCards from "./Components/TestimonialCards/TestimonialCards.jsx";
+import Footer from "./Components/Footer/Footer.jsx"
 function App() {
   return(
     <>
       <Home/>
       <One/>
-      {/* <TestimonialCards/> */}
       <Testimonial/>
+      <Footer/>
     </>
   );
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,49 +3,48 @@ import './Footer.css'
 
 export default function Footer(){
     return (
-        <>
-        <footer>
-        <div id="container" class="common">
-            <ul class="links-list">
-                    <li class="links-list-title">Company</li>
-                    <li class="links-list-items"><a href="">About us</a></li>
-                    <li class="links-list-items"><a href="">Press</a></li>
-                    <li class="links-list-items"><a href="">Careers</a></li>
-                    <li class="links-list-items"><a href="">Engineering</a></li>
-                    <li class="links-list-items"><a href="">Accessibility</a></li>
+        <div className='footer'>
+
+        <div id="container" className="common">
+            <ul className="links-list">
+                    <li className="links-list-title">Company</li>
+                    <li className="links-list-items"><a href="/">About us</a></li>
+                    <li className="links-list-items"><a href="/">Press</a></li>
+                    <li className="links-list-items"><a href="/">Careers</a></li>
+                    <li className="links-list-items"><a href="/">Engineering</a></li>
+                    <li className="links-list-items"><a href="/">Accessibility</a></li>
             </ul>
-            <ul class="links-list">
-                    <li class="links-list-title">Resources</li>
-                    <li class="links-list-items"><a href="">Big Ideas</a></li>
-                    <li class="links-list-items"><a href="">Points</a></li>
-                    <li class="links-list-items"><a href="">Resources</a></li>
-                    <li class="links-list-items"><a href="">Training</a></li>
-                    <li class="links-list-items"><a href="">Remote Learning</a></li>
+            <ul className="links-list">
+                    <li className="links-list-title">Resources</li>
+                    <li className="links-list-items"><a href="/">Big Ideas</a></li>
+                    <li className="links-list-items"><a href="/">Points</a></li>
+                    <li className="links-list-items"><a href="/">Resources</a></li>
+                    <li className="links-list-items"><a href="/">Training</a></li>
+                    <li className="links-list-items"><a href="/">Remote Learning</a></li>
             </ul>
-            <ul class="links-list">
-                    <li class="links-list-title">Support</li>
-                    <li class="links-list-items"><a href="">Helpdesk</a></li>
-                    <li class="links-list-items"><a href="">Contact</a></li>
-                    <li class="links-list-items"><a href="">Cookies Setting</a></li>
-                    <li class="links-list-items"><a href="">Privacy Center</a></li>
+            <ul className="links-list">
+                    <li className="links-list-title">Support</li>
+                    <li className="links-list-items"><a href="/">Helpdesk</a></li>
+                    <li className="links-list-items"><a href="/">Contact</a></li>
+                    <li className="links-list-items"><a href="/">Cookies Setting</a></li>
+                    <li className="links-list-items"><a href="/">Privacy Center</a></li>
             </ul>
-            <ul class="links-list">
-                    <li class="links-list-title">Community</li>
-                    <li class="links-list-items"><a href="">Teacher Community</a></li>
-                    <li class="links-list-items"><a href="">Facebook</a></li>
-                    <li class="links-list-items"><a href="">Twitter</a></li>
-                    <li class="links-list-items"><a href="">Instagram</a></li>
-            </ul>
-        </div>
-        <div id="foot-nav">
-            <ul class="nav-list">
-                <li class="nav-list-items"><a href="">&copy;Pediaverse</a></li>
-                <li class="nav-list-items"><a href="">Terms of Service</a></li>
-                <li class="nav-list-items"><a href="">Product Privacy Policy</a></li>
-                <li class="nav-list-items"><a href="">Website Privacy Policy</a></li>
+            <ul className="links-list">
+                    <li className="links-list-title">Community</li>
+                    <li className="links-list-items"><a href="/">Teacher Community</a></li>
+                    <li className="links-list-items"><a href="/">Facebook</a></li>
+                    <li className="links-list-items"><a href="/">Twitter</a></li>
+                    <li className="links-list-items"><a href="/">Instagram</a></li>
             </ul>
         </div>
-    </footer>
-        </>
+        <div className='footer-navigation'>
+            <div className='footer-nav-list'>
+                <div className='footer-nav-list-item'><a href='/'>&copy;Pediaverse </a></div>
+                <div className='footer-nav-list-item'><a href="/">Terms of Service</a></div>
+                <div className='footer-nav-list-item'><a href="/">Product Privacy Policy</a></div>
+                <div className='footer-nav-list-item'><a href="/">Website Privacy Policy</a></div>
+            </div>
+        </div>
+        </div>
     );
 }
