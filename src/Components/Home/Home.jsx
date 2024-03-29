19c7dfcf3fd32cf4eb2d6eeb03c08cc0c9e3ce6c
71203mayank <mayank71203@gmail.com>
2024-03-29 18:17:19 +0530

    added animation in hero section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from './Home.module.css'
 import Navbar from '../Navbar/Navbar'
-
+import {motion} from 'framer-motion'
 
 export default function Home() {
   return (
     <div className={styles['home']}>
         <Navbar/>
-        <div className={styles['home-heading']}>
+        <motion.div
+          initial = {{x:'200px'}}
+          animate = {{x:0}}
+          transition={{duration: 0.5}}
+          className={styles['home-heading']}>
             <div className={styles['home-main-heading']}>Do what you love, Love what you do</div>
             <div className={styles['home-sub-heading']}>Discover and pursue your <span style={{color:'#28CD88'}}>passion</span> in life</div>
-        </div>
+        </motion.div>
 
         <div className={styles['home-foreground-container']}>
             <img className={styles['home-foreground']} src='./Assets/home-foreground.svg' alt='foreground'></img>
