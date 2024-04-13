58ddc35b6d3f55d84a301c6e5f397f1a3815340c
71203mayank <mayank71203@gmail.com>
2024-04-14 03:06:37 +0530

    made hybrid learn responsive

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,12 +8,19 @@ export default function About() {
             <div className={styles['our-mission']}>
                 Our mission is to unlock and cultivate the true potential of a child<br></br> right from the start. We understand the importance of consistent<br></br> guidance and support in helping children develop their skills and<br></br> explore their passions.
             </div>
+            <div className={styles['our-mission-mobile']}>
+                Our mission is to unlock and cultivate the true potential of a child right from the start. We understand the importance of consistent guidance and support in helping children develop their skills and explore their passions.
+            </div>
             <div className={styles['about-main-container']}>
                 <div className={styles['about-main-img-container-one']}>
                     <img className={styles['about-main-img']} src='./Assets/plain8.svg' alt='img'></img>
                 </div>
                 <div className={styles['about-main-text']}>
                     Everybody is a <span style={{color:'#28CD88'}}>Genius</span> . But If You Judge a Fish by Its Ability to<br></br>
+                    Climb a Tree, It Will Live Its Whole Life Believing that It is Stupid.
+                </div>
+                <div className={styles['about-main-text-mobile']}>
+                    Everybody is a <span style={{color:'#28CD88'}}>Genius</span>.<br></br> But If You Judge a Fish by Its Ability to
                     Climb a Tree, It Will Live Its Whole Life Believing that It is Stupid.
                 </div>
                 <div className={styles['about-main-img-container-two']}>
